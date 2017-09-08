a557504c217517c021d4e99f9ad13835201b1259
Chris Thompson <cjt7g6@umsl.edu>
2017-09-08 13:45:39 -0500

    Added a scatter plot feature

--- cjt7g6_proj1.py
+++ cjt7g6_proj1.py
@@ -16,25 +16,43 @@ def createData():
 
     randVals = [] #empty list for the random values
     for i in range(999):
-        x = random.randint(0, 500) #create a random number
+        x = random.randint(0, 150) #create a random number
         randVals.append(x) #append it to the list 1000 times
 
-    df['randVals'] = randVals #assign the list to the dataframe
+    df['Weight'] = randVals #assign the list to the dataframe
 
-    tagVals = [] #empty list for the tag values
-    for row in df['randVals']: #assigning binary classifiers to the pandas dataframe
-        if row > 327:
-            tagVals.append('Red') #assign red to the list element if greater than 327
+
+    height = [] #empty list for the tag values
+    classifier = [] #empty list for the classification
+    for row in df['Weight']: #assigning binary classifiers to the pandas dataframe
+        if row > 96:
+            height.append(random.randint(0, 50)) #make a random small height
+            classifier.append("Red") #make a classification
         else:
-            tagVals.append('Black') #otherwise assign black
+            height.append(random.randint(51, 100)) #make a random big height
+            classifier.append("Black") #make a classification
 
-    df['tagVals'] = tagVals #assign the list to the dataframe
+    df['Height'] = height #assign the list to the dataframe
+    df['Classification'] =classifier #assign the answers to the dataframe
+    print(df)
 
     return df
 
 def plotVals():
     df = createData()
-    plt.plot(df['randVals'])
+    x = df['Weight'].values
+    y = df['Height'].values
+    colors = ('orange', 'black')
+    groups = ("red", "black")
+
+    #Create a plot
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, facecolor='w')
+
+    for color, group in zip(colors, groups):
+        ax.scatter(x, y, alpha=0.5, c=color, edgecolors='none', s=25, label=group)
+
+    plt.title('Binary Classification')
     plt.show()
 
 plotVals()
