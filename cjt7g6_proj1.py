6a6e0d91b94e7d222f5fcc8fafa5e2b4c90d7c8f
Chris Thompson <cjt7g6@umsl.edu>
2017-09-11 22:25:45 -0500

    1) I corrected the divergence issue. PLA only requires that sign(D) == d. 2) I added stopping conditions for training the model. 3) I created a test function to test the correctness of the model.

--- cjt7g6_proj1.py
+++ cjt7g6_proj1.py
@@ -11,8 +11,8 @@ def createData():
 
     '''create some random values for x and y. if the y value > 0, tag it with a 1, otherwise tag it with a -1'''
     for i in range(1000):
-        x1 = random.uniform(-10.0, 10.0)
-        y1 = random.uniform(-10.0, 10.0)
+        x1 = random.uniform(-10, 10)
+        y1 = random.uniform(-10, 10)
         x.append(x1)
         y.append(y1)
 
@@ -30,6 +30,13 @@ def createData():
     df['Class'] = d
 
     return df
+
+'''this function will test whether the value is positive'''
+def isPos(val):
+    if val > 0:
+        return True
+    else:
+        return False
 
 '''This function will plot the linearly separable data'''
 def plotVals(df):
@@ -59,59 +66,103 @@ def weightsUpdate(weights, constantC, constantK, classificationd, x, y):
 
     return weights
 
+'''NOTE: Write in loop controls for training the model
+1) if the errorRate does not continually improve, spit out the model weights
+2) if the errorRate reaches 0, spit out the model weights
+3) TBD'''
+
 '''this function will train the weights to make a correct discriminant output'''
-def trainModel(df, weights, constantC, constantK, maxIter, threshHold):
+def trainModel(df, weights, constantC, constantK, maxIter):
     #grab the values in a list
     x = df['X'].values
     y = df['Y'].values
     d = df['Class'].values
+    globalErrorRate = 0
 
     #define some variables to keep track
     numTurns = 0
+    numWeightUpdates = 0
 
     while numTurns < maxIter:
-        errorRate = 0
+        localErrorRate = 0
+        successRate = 0
         falsePosNeg = 0
         truePosNeg = 0
 
-        '''assign som threshhold values. must accomodate for slight variance.'''
-        posThreshHoldCeiling = 1 + threshHold
-        posThreshHoldFloor = 1 - threshHold
-        negThreshHoldFloor = -1 - threshHold
-        negThreshHoldCeiling = -1 + threshHold
-
         for i in range(len(x)):
-            ''' calculate the discriminant D = w0 + w1*xi + w2*yi '''
+            #calculate the discriminant
             discriminant = weights[0] + (weights[1] * x[i]) + (weights[2] * y[i])
 
-            '''if the discriminant is not correct when compared to the correct output'''
-            if ((discriminant >= posThreshHoldFloor and discriminant <= posThreshHoldCeiling) or
-                    (discriminant >= negThreshHoldFloor and discriminant <= negThreshHoldCeiling)):
+            if(isPos(discriminant) and d[i] == 1):
                 truePosNeg += 1
-                #weights = weightsUpdate(weights, constantC, constantK, d[i], x[i], y[i])
-            else:
-                '''update the weights'''
-                weights = weightsUpdate(weights, constantC, constantK, d[i], x[i], y[i])
-                falsePosNeg += 1
 
+            elif(isPos(discriminant) == False and d[i] == -1):
+                truePosNeg += 1
+
+            else:
+                falsePosNeg += 1
+                weights = weightsUpdate(weights, constantC, constantK, d[i], x[i], y[i])
 
         numTurns += 1 #increase number of turns by 1 iteration
         print("Number of False Positive/Negative: " + str(falsePosNeg))
         print("Number of True Positive/Negative: " + str(truePosNeg))
-        errorRate = falsePosNeg / len(x) * 100
-        print("Error rate: " + str(errorRate) + "%")
+        localErrorRate = falsePosNeg / len(x) * 100
+        successRate = truePosNeg / len(x) * 100
+        print("Error rate: " + str(localErrorRate) + "%")
+        print("Success rate: " + str(successRate) + "%")
 
 
-        '''add stop conditions'''
-        if (errorRate < 25):
+        if successRate == 100:
+            print("\n\nTrained Weight Values: " + str(weights))
+            print("Number of iterations: " + str(numTurns))
             break
+
         else:
             continue
 
+    return weights
+
+'''this function will test the accuracy of the model with one pass through the dataset'''
+def testModel(weights):
+    testDF = createData() #create data to test the model with
+
+    #grab the values from the test data
+    x = testDF['X'].values
+    y = testDF['Y'].values
+    d = testDF['Class'].values
+
+    #create an empty list of predicted values
+    D = []
+    numErrors = 0
+    numCorrect = 0
+
+    for i in range(len(x)):
+        discriminant = weights[0] + (weights[1] * x[i]) + (weights[2] * y[i]) #make the prediction
+
+        D.append(discriminant) #append the value to the list
+
+    '''iterate through the list and let's see how we did'''
+    for x in range(len(d)):
+        if(isPos(D[i]) and d[i] == 1):
+            numCorrect += 1
+        elif(isPos(D[i]) == False and d[i] == -1):
+            numCorrect += 1
+        else:
+            numErrors += 1
+
+    print("\nNumber of errors on test data: " + str(numErrors))
+    print("Error Rate: " + str(numErrors / len(d)))
+
+    resultsDF = pd.DataFrame()
+    resultsDF['Predicted Output'] = D
+    resultsDF['Expected Output'] = d
+    print(resultsDF)
+
 def main():
-    weights = [0.1, 0.1, 0.1]
+    weights = [0.00000, 0.00000, 0.00000]
     df = createData()
     #plotVals(df) #just to show that the data is linearly separable
-    trainModel(df, weights, 0.0000001, 0.0000001, 2000000, 0.35)
+    weights = trainModel(df, weights, 0.01, 1, 1000000)
+    testModel(weights)
 
 main()
